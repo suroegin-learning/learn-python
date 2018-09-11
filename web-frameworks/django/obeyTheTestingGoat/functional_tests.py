e1a0b459dafcb9309313c94638947e5e3634fbdd
Ivan Suroegin <ivan.suroegin@gmail.com>
2018-09-11 15:11:17 +0300

    Add all modified and new files and directories.

---
+++ web-frameworks/django/obeyTheTestingGoat/functional_tests.py
@@ -0,0 +1,71 @@
+"""
+First version, very simple.
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+# Ivan goes to checkout his homepage
+browser.get("http://localhost:8000")
+
+try:
+    # Ivan noticed the page title and header mention To-Do lists
+    assert 'To-Do' in browser.title, "Browser title was " + browser.title
+finally:
+    # Ivan is invited to enter a to-do item straight away
+    browser.quit()
+"""
+
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+
+    def tearDown(self):
+        self.browser.close()
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # Ivan is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
+
+        # Ivan types "Buy peacock feathers" into a text box
+        inputbox.send_keys("Buy peacock feathers")
+
+        # Ivan hits enter key
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacocks deathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacocks deathers to make a fly')
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
